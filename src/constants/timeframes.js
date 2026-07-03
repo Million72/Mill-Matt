3539b7a7abe1c4408771301e54701339a00930b5
Million72 <135624698+Million72@users.noreply.github.com>
2026-07-03 13:47:20 +0200

    Update timeframes.js

--- src/constants/timeframes.js
+++ src/constants/timeframes.js
@@ -1,7 +1,8 @@
 export const TIMEFRAMES = {
   "1m":  { granularity: 60,    htfGran: 300,   candles: 300, label: "1m"  },
-  "5m":  { granularity: 300,   htfGran: 3600,  candles: 300, label: "5m"  },
-  "15m": { granularity: 900,   htfGran: 3600,  candles: 300, label: "15m" },
+  "5m":  { granularity: 300,   htfGran: 900,   candles: 300, label: "5m"  },
+  "15m": { granularity: 900,   htfGran: 1800,  candles: 300, label: "15m" },
+  "30m": { granularity: 1800,  htfGran: 3600,  candles: 300, label: "30m" },
   "1h":  { granularity: 3600,  htfGran: 14400, candles: 300, label: "1h"  },
   "4h":  { granularity: 14400, htfGran: 86400, candles: 300, label: "4h"  },
 };
